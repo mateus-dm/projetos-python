c7f4da8efac42b719ee5691d7d18a12866f095c2
Mateus Dantas Monico <mateussd2@gmail.com>
2022-09-27 18:07:49 -0300

    Função adicionada
    Função para escolher a dificuldade e melhorar o código foi melhorada.

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,20 +4,8 @@ def jogar():
 
     introducao()
     numero_secreto = random.randrange(1, 100)
-    total_de_tentativas = 0
+    total_de_tentativas = escolher_dificuldade()
     pontos = 1000
-
-    print("Qual o nível de dificuldade?")
-    print("(1) Fácil (2) Médio (3) Difícil")
-
-    nivel = int(input("Defina o nível: "))
-
-    if (nivel == 1):
-        total_de_tentativas = 20
-    elif (nivel == 2):
-        total_de_tentativas = 10
-    else:
-        total_de_tentativas = 5
 
     for rodada in range(1, total_de_tentativas + 1):
         print(f"Tentativa {rodada} de {total_de_tentativas}")
@@ -42,8 +30,8 @@ def jogar():
                 print("Você errou! O seu chute foi maior que o número secreto.")
             elif (menor):
                 print("Você errou! O seu chute foi menor que o número secreto.")
-            pontos_perdidos = abs(numero_secreto - chute)
-            pontos = pontos - pontos_perdidos
+            pontos_perdidos = abs(numero_secreto - chute) 
+            pontos = pontos - pontos_perdidos #toda vez que o usuário erra um chute, a diferença entre o número do chute e o número random é diminuída da quantidade total de pontos. 
 
     print("Fim do jogo")
 
@@ -51,6 +39,20 @@ def introducao():
     print("*********************************")
     print("Bem vindo ao jogo de Adivinhação!")
     print("*********************************")
+
+def escolher_dificuldade():
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
+
+    nivel = int(input("Defina o nível: "))
+
+    if (nivel == 1):
+        total_de_tentativas = 20
+    elif (nivel == 2):
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+    return total_de_tentativas  
 
 if (__name__ == "__main__"):
     jogar()
